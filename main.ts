03326a0ff9a3fb85964fefae1438778c2006c507
Raulif <raul4cade@gmail.com>
2025-11-17 16:26:53 +0100

    Change request handler

--- main.ts
+++ main.ts
@@ -1,29 +1,44 @@
-import { Application } from 'jsr:@oak/oak/application';
-import { Router } from 'jsr:@oak/oak/router';
+import { Resend } from "npm:resend";
 
-import { handleContact } from './handle-contact.ts';
+const getTemplate = ({ name, email, phone, message }: { name: string, email: string, phone: string, message: string }) => `
+<div>
+  <p>${name ?? 'A user'} sent a message.</p>
+  <p>Message:</p>
+  <p>${message ?? ''}</p>
+  <p>Contact Info:</p>
+  ${email ? `<p>Email Address: <a href="mailto:${email}">${email}</a></p>` : '<p>Email Address: No email provided</p>'}
+  ${phone ? `<p>Phone: <a href="tel:${email}">${email}</a></p>` : '<p>Phone: No phone provided</p>'}
+</div>
+`
 
-const app = new Application();
-app.use((ctx, next) => {
-  ctx.response.headers.set('Access-Control-Allow-Origin', '*');
-  return next();
+Deno.serve(async (_req) => {
+  try {
+    const resendApiKey = Deno.env.get('RESEND_API_KEY') as string;
+    const resend = new Resend(resendApiKey);
+    const body = await _req.json()
+
+    if (body.occupation) return new Response(null, { status: 200 })
+    if (body.email) {
+
+      const response = await resend.emails.send({
+        from: 'Yo-Jo Contact Form <contact@yojo-cologne.de>',
+        to: ['raul4cade@gmail.com'],
+        subject: 'Yo-Jo Contact Form',
+        html: getTemplate(body)
+      });
+      console.log({ response })
+      return new Response(JSON.stringify(response), {
+        status: response.error ? 500 : 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+    return new Response(null, { status: 200 })
+  } catch (error) {
+    console.error(error);
+    return new Response(null, {
+      status: 500,
+    });
+  }
 });
-const router = new Router();
-router.post('/contact', handleContact);
-router.get('/test', (ctx) => {
-  console.log('test');
-  ctx.response.body = { ok: true };
-
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-app.addEventListener('listen', ({ hostname, port, secure }) => {
-  console.log(
-    `Listening on: ${secure ? 'https://' : 'http://'}${hostname ?? 'localhost'
-    }:${port}`
-  );
-});
-
-await app.listen({ port: 8080 });
